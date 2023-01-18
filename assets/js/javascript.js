766e12d971eed16d4dd64b1041dc96f15ae3e99b
Brenan Corcoran <bcorcoran1@luc.edu>
2023-01-17 17:45:53 -0700

    added header to portfolio, still need to update content

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,4 +1,12 @@
 function togglePopup() {
     document.getElementById("popup-1").classList.toggle("active");
-    
+
+}
+
+function openNav() {
+  document.getElementById('tr-sidenav').style.width = '250px';
+}
+
+function closeNav() {
+  document.getElementById('tr-sidenav').style.width = '0';
 }
